34c6d3a8cb698d299fcaf6696e1322239f369f44
Downey03 <44365074+Downey03@users.noreply.github.com>
2023-07-14 20:13:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+funtion change(){
+	return new Promise.resolve("Hello, World!")
+}
+setTimeout(()=>{
+	let x = change();
+	x= "hel"
+	document.getElementById("output").textContent = await x.json()
+},1000)
